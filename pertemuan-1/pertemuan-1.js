d012db18deefb8e6de323335c3ffbbab057fec06
Messi2026 <muhammadidrisramzah@gmail.com>
2025-08-12 22:04:19 +0700

    Pertemuan Pertama

---
+++ pertemuan-1/pertemuan-1.js
@@ -0,0 +1,30 @@
+const nama = "Idris Mikropipet"
+const umur = 19
+const asal = "asli PRIBUMi"
+
+
+
+console.log(nama, umur, asal)
+
+
+
+console.log(20/2)
+
+let angka1 = 2
+let angka2 = 3
+const angka3 = 5
+console.log(angka1+angka2/angka3)
+
+angka1 = 4
+angka2= 7
+console.log(angka1*angka2/angka3)
+
+const input1 = 21
+
+if(input1 % 2 === 2){
+
+    console.log ("input1=genap")
+}
+else {
+    console.log ("False")
+}
---
+++ pertemuan-1/soal-pertemuan-1.js
@@ -0,0 +1,36 @@
+// Apakah Angka Positif?
+let angka1 = 1
+angka1 = Number(angka1)
+
+
+if (angka1>0){
+    console.log ("Positif Covid 19")}
+else if (angka1<0){
+    console.log ("Niggatif")
+}
+else if (angka1==0){
+    console.log ("Nol awaowkaowka")
+}
+else {
+    console.log ("error")
+}
+
+// Apakah <, >, =
+let number1 = 1000000
+let number2 = 500000
+number1 = Number(number1)
+number2 = Number(number2)
+
+if (number1 > number2)
+    console.log (`${number1} kemahalan le mending ${number2} lebih murah`)
+else if (number1 < number2)
+    console.log (`${number1} sudah pasti murah le men nak ${number2} lajulah`)
+else if (number1 === number2)
+    console.log (`${number1} samo bae hargonyo dengen ${number2}`)
+else {
+    console.log ("error")
+}
+
+
+
+
